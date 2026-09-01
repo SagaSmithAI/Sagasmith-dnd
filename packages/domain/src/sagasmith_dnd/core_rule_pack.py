71abb89d0f9e6b997a11c17ccb2d00ad8dfbcd21
dajiaohuang <mikewushuwen@outlook.com>
2026-09-01 19:28:11 +0800

    chore: relock official expansions for Tortle armor

--- packages/domain/src/sagasmith_dnd/core_rule_pack.py
+++ packages/domain/src/sagasmith_dnd/core_rule_pack.py
@@ -9,7 +9,7 @@ from sagasmith_core.integrity import json_sha256
 
 from sagasmith_dnd.editions import SUPPORTED_DND_EDITIONS, normalize_dnd_edition
 
-CORE_RULE_PACK_VERSION = "1.73.0"
+CORE_RULE_PACK_VERSION = "1.74.0"
 
 
 @dataclass(frozen=True)
